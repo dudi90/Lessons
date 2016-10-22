0aebed2ab4606e2da8e1f3b0c152ead617baa75f
Tomek Dudek <ttomekk0@gmail.com>
2016-10-22 10:49:40 +0200

    Homework

--- src/korepetycje1/Kalkulator.java
+++ src/korepetycje1/Kalkulator.java
@@ -1,162 +1,156 @@
 package korepetycje1;
 
-
 import java.util.Scanner;
 
-public class Kalkulator {
+public class Kalkulator
+{
     public static void main(String[] args) {
-
-
-        int result=factorial(-10);
-        System.out.println(result);
-
-
         double a = 0;
         double b = 0;
+        int d=0;
         String c;
-        String[] menu = {"1","2","3","4"};
+        String[] menu = {"1", "2", "3", "4", "5"};
 
-        Scanner kalkulator = new Scanner(System.in);
-        System.out.println("Podaj liczbę nr 1");
-        a = kalkulator.nextDouble();
-        System.out.println("Podaj liczbę nr 2");
-        b = kalkulator.nextDouble();
-
-
-        System.out.println("Możliwe działania: " + "(Wybierz numer działania)");
-        System.out.println("Dodawanie- " + "1");
-        System.out.println("Odejmowanie- " + "2");
-        System.out.println("Mnożenie- " + "3");
-        System.out.println("Dzielenie- " + "4");
-
-        c = kalkulator.next();
-
-        double wynik=0.0;
-
-        if(c.equals(menu[0]))
+        System.out.println("Calculator. Select an operation by pressing right number");
+        System.out.println("Addition - " + "      1");
+        System.out.println("Subtraction - " + "   2");
+        System.out.println("Multiplication - " + "3");
+        System.out.println("Division - " + "      4");
+        System.out.println("Factorial - " + "     5");
+        Scanner calculator = new Scanner(System.in);
+        System.out.println("Enter operation number:");
+        c = calculator.next();
+        double result = 0.0;
+        if (c.equals(menu[0]))
         {
-            wynik = dodawanie(a,b);
+            System.out.println("Enter number 1:");
+            a = calculator.nextDouble();
+            System.out.println("Enter number 2:");
+            b = calculator.nextDouble();
+            result = addition(a, b);
         }
-        else if(c.equals(menu[1]))
-
+        else if (c.equals(menu[1]))
         {
-            wynik = odejmowanie(a,b);
+            System.out.println("Enter number 1");
+            a = calculator.nextDouble();
+            System.out.println("Enter number 2");
+            b = calculator.nextDouble();
+            result = subtraction(a, b);
         }
-        else if(c.equals(menu[2]))
+        else if (c.equals(menu[2]))
         {
-            wynik = mnożenie(a,b);
+            System.out.println("Enter number 1");
+            a = calculator.nextDouble();
+            System.out.println("Enter number 2");
+            b = calculator.nextDouble();
+            result = multiplication(a, b);
         }
-        else if(c.equals(menu[3]))
+        else if (c.equals(menu[3]))
         {
-            wynik = dzielenie(a,b);
-            if (a==0)
-            {
-                System.out.println("Nie dzielimy przez 0");
-                return;
-            }
-            if (b==0)
-            {
-                System.out.println("Nie dzielimy przez 0");
-                return;
-            }
+            System.out.println("Enter number 1");
+            a = calculator.nextDouble();
+            System.out.println("Enter number 2");
+            b = calculator.nextDouble();
+            result = division(a, b);
+        }
+        else if (c.equals(menu[4]))
+        {
+            System.out.println("Enter natural number");
+            d = calculator.nextInt();
+            result = factorial(d);
         }
         else
-            System.out.println("Podano wartość nieobsługiwaną");
-
-        System.out.println("Wynik działania to: " + wynik);
-
-
+        {
+            System.out.println("Wrong data");
+        }
+        System.out.println("Result: " + result);
     }
-
-    public static double dodawanie (double a, double b)
+    public static double addition (double a, double b)
     {
         double wynik = a+b;
         return wynik;
     }
-    public static double odejmowanie (double a, double b)
+    public static double subtraction (double a, double b)
     {
         double wynik = a-b;
         return wynik;
     }
-    public static double mnożenie (double a, double b)
+    public static double multiplication (double a, double b)
     {
         double wynik = a*b;
         return wynik;
     }
-    public static double dzielenie (double a, double b)
+    public static double division(double a, double b)
     {
-        double wynik = a/b; // zanim podzielisz, sprawdź czy na pewno możesz podzielić
-        return wynik;
-    }
-
-    public static int factorialFor ()
-    {
-        for (int i=1; i!=11 ; i++ )
+        if (b == 0)
         {
-            System.out.println(i);
-        }
-        return 0;
-    }
-
-    public static int factorialWhile ()
-    {
-        int i=1;
-        while (i<11)
-        {
-            i++;
-            System.out.println(i);
-
-        }
-        return 0;
-
-    }
-    public static int factorialDoWhile ()
-    {
-        int i= 12;
-        do
-        {
-            i++;
-            System.out.println(i);
-
-        } while (i<11);
-        return 0;
-    }
-
-    public static int factorial (int number)
-    {
-        int result=1;
-        if (number<0)
-        {
-            System.out.println("Wrong data");
-            result = 0;
+            System.out.println("Do not divide by 0");
+            return 0;
         }
         else
         {
-            for (int i=1; i<number+1; i++)
+            double wynik = a / b;
+            return wynik;
+        }
+    }
+//    public static int factorialFor ()
+//    {
+//        for (int i=1; i!=11 ; i++ )
+//        {
+//            System.out.println(i);
+//        }
+//        return 0;
+//    }
+//
+//    public static int factorialWhile ()
+//    {
+//        int i=1;
+//        while (i<11)
+//        {
+//            i++;
+//            System.out.println(i);
+//
+//        }
+//        return 0;
+//
+//    }
+//    public static int factorialDoWhile ()
+//    {
+//        int i= 12;
+//        do
+//        {
+//            i++;
+//            System.out.println(i);
+//
+//        } while (i<11);
+//        return 0;
+//    }
+//
+    public static int factorial (int d)
+    {
+        int result = 1;
+        if (d < 0) {
+            System.out.println("Wrong data");
+            result = 0;
+        } else
+        {
+            for (int i = 1; i < d + 1; i++)
             {
-                result=result*i;
+                result = result * i;
             }
         }
         return result;
     }
-    public static void arrayItteration ()
-    {
-        String[] names = {"Jan", "Ania","Piotr", "Krzsztof"};
-
-        for (int i=0; i<names.length; i++)
-        {
-            System.out.println(names[i]);
-        }
-
-
+//    }
+//    public static void arrayItteration ()
+//    {
+//        String[] names = {"Jan", "Ania","Piotr", "Krzsztof"};
+//
+//        for (int i=0; i<names.length; i++)
+//        {
+//            System.out.println(names[i]);
+//        }
     }
-
-
-
-
-}
-
-
 
 // Poprawienie dzielenia
 // Dodać factorial do kalkulatora
